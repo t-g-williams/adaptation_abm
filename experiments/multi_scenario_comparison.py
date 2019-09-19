8bc56bfd953faa304642b37b24009179978d5abb
t-g-williams <tgw@umich.edu>
2019-09-19 10:05:02 -0400

    added shock simulation

--- experiments/multi_scenario_comparison.py
+++ experiments/multi_scenario_comparison.py
@@ -10,6 +10,7 @@ import calibration.POM as POM
 import imp
 import copy
 import code
+import tqdm
 import numpy as np
 import pickle
 import copy
@@ -18,8 +19,8 @@ from tqdm import tqdm
 import multiprocessing
 
 def main():
-    nreps = 1000
-    exp_name = 'MS_1000yrs'
+    nreps = 10000
+    exp_name = 'MS_100yrs_shock'
     ncores = 40
 
     # load default params
@@ -29,25 +30,34 @@ def main():
     f = '../outputs/POM/constrained livestock frac/input_params_0.pkl'
     inp_base = pickle.load(open(f, 'rb'))
     # manually specify some variables (common to all scenarios)
-    T = 1000
+    T = 100
     inp_base['model']['T'] = T
-    inp_base['model']['n_agents'] = 200
+    inp_base['model']['n_agents'] = 100
     inp_base['model']['exp_name'] = exp_name
     inp_base['agents']['adap_type'] = 'always'
     inp_base['adaptation']['insurance']['cost_factor'] = 1 # remove in future -- only cause not in POM experiment
+    # inp_base['agents']['n_plots_init'] = [15,16,17,18,19]
 
-    # define some scenarios
+    #### adaptation scenarios
     scenarios = {
         'baseline' : {'model' : {'adaptation_option' : 'none'}},
         'insurance' : {'model' : {'adaptation_option' : 'insurance'}},
         'cover_crop' : {'model' : {'adaptation_option' : 'cover_crop'}},
     }
+    shock_years = []
+
+    #### shock scenarios
+    scenarios = {
+        'baseline' : {'model' : {'adaptation_option' : 'none', 'shock' : False}},
+        'shock' : {'model' : {'adaptation_option' : 'none', 'shock' : True}, 'climate' : {'shock_years' : [15], 'shock_rain' : 0.1}},
+    }
+    shock_years = [15]
 
     #### RUN THE MODELS ####
     mods = multi_mod_run(nreps, inp_base, scenarios, ncores)
 
     #### PLOT ####
-    plt_multi.main(mods, nreps, inp_base, scenarios, exp_name, T)
+    plt_multi.main(mods, nreps, inp_base, scenarios, exp_name, T, shock_years)
 
 def multi_mod_run(nreps, inp_base, scenarios, ncores):
     mods = {}
@@ -63,11 +73,12 @@ def multi_mod_run(nreps, inp_base, scenarios, ncores):
 
         # format the outputs
         mods[name] = {
-            'wealth' : np.array([oi for tmp_i in tmp for oi in tmp_i['wealth']]),
-            'organic' : [oi for tmp_i in tmp for oi in tmp_i['organic']], # each rep is different length
+            'wealth' : np.array([oi for tmp_i in tmp for oi in tmp_i['wealth']]).astype(int),
+            'organic' : [oi.astype(int) for tmp_i in tmp for oi in tmp_i['organic']], # each rep is different length
             'n_plots' : np.array([oi for tmp_i in tmp for oi in tmp_i['n_plots']]),
             'coping' : np.array([oi for tmp_i in tmp for oi in tmp_i['coping']]),
             'owners' : [oi for tmp_i in tmp for oi in tmp_i['owners']],
+            'income' : np.array([oi for tmp_i in tmp for oi in tmp_i['income']]).astype(int),
         }
 
     return mods
@@ -77,7 +88,9 @@ def run_chunk_reps(reps, params):
     run a chunk of replications
     '''
     params = copy.copy(params)
-    ms = {'wealth' : [], 'organic' : [], 'coping' : [], 'n_plots' : [], 'owners' : []}
+    ms = {'wealth' : [], 'organic' : [], 'coping' : [], 'n_plots' : [], 'owners' : [],
+        'income' : []}
+    # with tqdm(reps, disable = not True) as pbar:
     for r in reps:
         params['model']['seed'] = r # set the seed
         
@@ -91,6 +104,8 @@ def run_chunk_reps(reps, params):
         ms['n_plots'].append(m.agents.n_plots)
         ms['coping'].append(m.agents.coping_rqd)
         ms['owners'].append(m.land.owner)
+        ms['income'].append(m.agents.income)
+        # pbar.update()
 
     return ms
 
--- plot/multi_scenario.py
+++ plot/multi_scenario.py
@@ -14,7 +14,7 @@ styles['plot_type'] = plot_type
 plt.style.use('fivethirtyeight')
 plt.style.use(styles[plot_type])
 
-def main(mods, nreps, inp_base, scenarios, exp_name, T):
+def main(mods, nreps, inp_base, scenarios, exp_name, T, shock_years=[]):
     '''
     plot each agent type (number of plots) separately
     this assumes there's 3 agent types
@@ -23,11 +23,14 @@ def main(mods, nreps, inp_base, scenarios, exp_name, T):
     if not os.path.isdir(savedir):
         os.makedirs(savedir)
 
-    combined_wealth_income(mods, nreps, inp_base, scenarios, exp_name, T, savedir)
-    neg_wealth_probabilities(mods, nreps, inp_base, scenarios, exp_name, T, savedir)
-    agent_trajectories(mods, nreps, inp_base, scenarios, exp_name, T, savedir, 'wealth')
-    agent_trajectories(mods, nreps, inp_base, scenarios, exp_name, T, savedir, 'income')
-    first_round_plots(mods, nreps, inp_base, scenarios, exp_name, T, savedir)
+    if len(shock_years) == 0:
+        # only run these for the adaptation scenarios -- this assumes the length of shock years here is zero
+        combined_wealth_income(mods, nreps, inp_base, scenarios, exp_name, T, savedir)
+        neg_wealth_probabilities(mods, nreps, inp_base, scenarios, exp_name, T, savedir)
+        agent_trajectories(mods, nreps, inp_base, scenarios, exp_name, T, savedir, 'wealth')
+        agent_trajectories(mods, nreps, inp_base, scenarios, exp_name, T, savedir, 'income')
+    
+    first_round_plots(mods, nreps, inp_base, scenarios, exp_name, T, savedir, shock_years)
 
 def neg_wealth_probabilities(mods, nreps, inp_base, scenarios, exp_name, T, savedir):
     '''
@@ -223,21 +226,24 @@ def agent_trajectories(mods, nreps, inp_base, scenarios, exp_name, T, savedir, p
         fig2.savefig(savedir + 'timeseries_{}_trajectories_{}_plots.png'.format(plt_type, nplot))
         plt.close('all')
 
-def first_round_plots(mods, nreps, inp_base, scenarios, exp_name, T, savedir):
+def first_round_plots(mods, nreps, inp_base, scenarios, exp_name, T, savedir, shock_years=[]):
     colors = ['b','r','k','g','y']
-    fig = plt.figure(figsize=(12,4))
+    fig = plt.figure(figsize=(18,6))
     axs = [fig.add_subplot(131),fig.add_subplot(132),fig.add_subplot(133)]
-    fig2 = plt.figure(figsize=(12,4))
+    fig2 = plt.figure(figsize=(18,6))
     ax2s = [fig2.add_subplot(131),fig2.add_subplot(132),fig2.add_subplot(133)]
-    fig3 = plt.figure(figsize=(12,4))
+    fig3 = plt.figure(figsize=(18,6))
     ax3s = [fig3.add_subplot(131),fig3.add_subplot(132),fig3.add_subplot(133)]
+    fig4 = plt.figure(figsize=(18,6))
+    ax4s = [fig4.add_subplot(131),fig4.add_subplot(132),fig4.add_subplot(133)]
 
     ii = 0
     for m, mod in mods.items():
 
         ## generate the plot data
         ## we want to take time-averages for each agent type
-        out_dict = {'wealth' : np.full(T+1, np.nan), 'SOM' : np.full(T+1, np.nan), 'coping' : np.full(T, np.nan)}
+        out_dict = {'wealth' : np.full(T+1, np.nan), 'SOM' : np.full(T+1, np.nan), 'coping' : np.full(T, np.nan),
+            'income' : np.full(T, np.nan)}
         ag_data = [copy.copy(out_dict), copy.copy(out_dict), copy.copy(out_dict)]
         for r in range(nreps):
             # find agent types
@@ -248,40 +254,48 @@ def first_round_plots(mods, nreps, inp_base, scenarios, exp_name, T, savedir):
             # add their data to the dictionaries
             for a, ag in enumerate(ags):
                 ag_data[a]['wealth'] = np.nanmean(np.array([ag_data[a]['wealth'], np.mean(mod['wealth'][r,:,ag], axis=0)]), axis=0)
+                ag_data[a]['income'] = np.nanmean(np.array([ag_data[a]['income'], np.mean(mod['income'][r,:,ag], axis=0)]), axis=0)
                 ag_data[a]['coping'] = np.nanmean(np.array([ag_data[a]['coping'], np.mean(mod['coping'][r,:,ag], axis=0)]), axis=0)
                 # find the land-level agent types
                 lan = np.in1d(mod['owners'][r], np.arange(ag.shape[0])[ag])
                 ag_data[a]['SOM'] = np.nanmean(np.array([ag_data[a]['SOM'], np.mean(mod['organic'][r][:,lan], axis=1)]), axis=0)
-
 
         ## PLOT ##
         for a, ag in enumerate(ags):
             axs[a].plot(ag_data[a]['wealth'], label=m, color=colors[ii])
             ax2s[a].plot(ag_data[a]['coping'], label=m, color=colors[ii])
             ax3s[a].plot(ag_data[a]['SOM'], label=m, color=colors[ii])
+            ax4s[a].plot(ag_data[a]['income'], label=m, color=colors[ii])
 
         ii += 1
+    
     # some formatting
     for a in range(3):
         axs[a].set_title('Wealth : agent type {}'.format(a+1))
         ax2s[a].set_title('Coping : agent type {}'.format(a+1))
         ax3s[a].set_title('SOM : agent type {}'.format(a+1))
+        ax4s[a].set_title('Income : agent type {}'.format(a+1))
         axs[a].set_xlabel('Time (yrs)')
         ax2s[a].set_xlabel('Time (yrs)')
         ax3s[a].set_xlabel('Time (yrs)')
         axs[a].set_ylabel('Birr')
         ax2s[a].set_ylabel('P(coping rqd)')
         ax3s[a].set_ylabel('kg/ha')
-        axs[a].legend()
-        ax2s[a].legend()
-        ax3s[a].legend()
+        ax3s[a].set_ylabel('Birr')
         axs[a].axhline(y=0, color='k', ls=':')
-        axs[a].grid(False)
-        ax2s[a].grid(False)
-        ax3s[a].grid(False)
+        for axx in [axs[a], ax2s[a], ax3s[a], ax4s[a]]:
+            axx.legend()
+            axx.grid(False)
+            axx.set_xlabel('Time (yrs)')
+            # show the shock on the plot, if necessary
+            if len(shock_years) > 0:
+                for yr in shock_years:
+                    axx.axvline(x=yr, color='k', ls=':')
+                    axx.text(yr, axx.get_ylim()[0]+(axx.get_ylim()[1]-axx.get_ylim()[0])*0.9, 'SHOCK', ha='center', rotation=90)
 
     fig.savefig(savedir + 'type_wealth.png')
     fig2.savefig(savedir + 'type_coping.png')
     fig3.savefig(savedir + 'type_SOM.png')
+    fig4.savefig(savedir + 'type_income.png')
     plt.close('all')
     # code.interact(local=dict(globals(), **locals()))
